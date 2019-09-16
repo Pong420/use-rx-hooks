92e2115fb20f06f823d07cb1ce2eb37190221ebf
Pong <samfunghp@gmail.com>
2019-09-16 13:50:55 +0800

    fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,11 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.eslint.json',
-  },
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
-    'prettier/react',
-    'prettier/@typescript-eslint',
-  ],
+  extends: 'react-app',
   env: {
     node: true,
     browser: true,
     jest: true,
   },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-angle-bracket-type-assertion': 0,
   },
 };
