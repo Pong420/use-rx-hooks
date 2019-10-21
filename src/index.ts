c47bd1d81bee51c4c8dc240457503e3065aec645
Pong <samfunghp@gmail.com>
2019-10-21 17:52:17 +0800

    fix export useRxInputComposing and useRxScroll

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './useRxInput';
 export * from './useRxFileToImage';
 export * from './useRxDropImage';
 export * from './useRxPasteImage';
+export * from './useRxInputComposing';
+export * from './useRxScroll';
